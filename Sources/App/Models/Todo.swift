44b4b0274e11408f44f0af5d69db3a50bfcb9c0a
Rui Peres <sauron.is.alive@gmail.com>
2019-02-28 11:57:24 +0000

    Removed Fluent

--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -1,8 +1,7 @@
-import FluentSQLite
 import Vapor
 
 /// A single entry of a Todo list.
-final class Todo: SQLiteModel {
+final class Todo {
     /// The unique identifier for this `Todo`.
     var id: Int?
 
@@ -16,11 +15,5 @@ final class Todo: SQLiteModel {
     }
 }
 
-/// Allows `Todo` to be used as a dynamic migration.
-extension Todo: Migration { }
-
 /// Allows `Todo` to be encoded to and decoded from HTTP messages.
 extension Todo: Content { }
-
-/// Allows `Todo` to be used as a dynamic parameter in route definitions.
-extension Todo: Parameter { }
