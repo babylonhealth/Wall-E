aa26d19dea23c398e5d56f06b7e62758003a002c
Rui Peres <RuiAAPeres@users.noreply.github.com>
2019-02-28 12:20:39 +0000

    Vapor bootstrap (#4)
    * Updated gitignore to match our internal ios-distribution
    
    * vapor new + vapor xcode
    
    * Following the same setup as we have for the internal distribution
    
    * Missing new line
    
    * Keeping the LinuxMain located in the testing folders
    
    * Removed Fluent
    
    * ran `vapor update` to remove fluent from Package.resolved

---
+++ Sources/App/Controllers/TodoController.swift
@@ -0,0 +1,6 @@
+import Vapor
+
+/// Controls basic CRUD operations on `Todo`s.
+final class TodoController {
+
+}
---
+++ Sources/App/configure.swift
@@ -0,0 +1,16 @@
+import Vapor
+
+/// Called before your application initializes.
+public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+
+    // Register routes to the router
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+
+    // Register middleware
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    services.register(middlewares)
+}
