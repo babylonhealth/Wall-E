cc9e40f7101d5c040a216adc33b17dd5a954a7b6
David Rodrigues <david.ndh@gmail.com>
2019-07-26 18:19:52 +0100

    Remove leftover

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "5.0.0"),
         .package(url: "https://github.com/Babylonpartners/ReactiveFeedback", from: "0.6.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "Bot", dependencies: ["ReactiveSwift", "ReactiveFeedback", "CryptoSwift"]),
