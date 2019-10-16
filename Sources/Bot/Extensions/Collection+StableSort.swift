768edf4756a7371da6b5e11429c69f1d950a91f9
Olivier Halligon <olivier.halligon@babylonhealth.com>
2019-10-16 22:56:03 +0200

    Updating test manifest
    swift test --generate-linuxmain

--- Sources/Bot/Extensions/Collection+StableSort.swift
+++ Sources/Bot/Extensions/Collection+StableSort.swift
@@ -2,11 +2,12 @@ import Foundation
 
 extension Collection {
     func slowStableSort(using predicate: (Self.Element) -> Bool) -> [Self.Element] {
-        // Note: sadly sort() and `partition(by:)` are not guaranteed to be stable sort according to doc,
+        // Note: sadly sort() and `partition(by:)` are not guaranteed to be stable according to doc,
         // so we can't use them here until we have stablePartition(by:)
         // https://github.com/apple/swift/blob/master/test/Prototypes/Algorithms.swift#L562
-        // This solution below is not the most efficient algorithm (memory-alloc-wise)
-        // but for our arrays of likely < 20 items it should be enough.
+        //
+        // The simpler solution below is not the most efficient algorithm (memory-alloc-wise),
+        // but for our arrays of likely < 20 items it should be ok.
         let triaged = Dictionary(grouping: self, by: predicate)
         let trueSlice = triaged[true] ?? []
         let falseSlice = triaged[false] ?? []
