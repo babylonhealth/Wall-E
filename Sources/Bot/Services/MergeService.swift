7cf5d6a681f0ecb3e92b4262d4fd4223e23a9269
Olivier Halligon <olivier.halligon@babylonhealth.com>
2019-10-16 17:33:56 +0200

    Better queue description with TP info

--- Sources/Bot/Services/MergeService.swift
+++ Sources/Bot/Services/MergeService.swift
@@ -731,7 +731,8 @@ extension MergeService.State: CustomDebugStringConvertible {
         let pullRequestsSeparator = "\n\t\t"
 
         let pullRequestsRepresentation = pullRequests.enumerated().map { index, pullRequest in
-            return "#\(index + 1): \(pullRequest)"
+            let isTP = pullRequest.isLabelled(withOneOf: self.topPriorityLabels)
+            return "#\(index + 1): \(pullRequest) \(isTP ? "[TP]" : "")"
         }.joined(separator: pullRequestsSeparator)
 
         return "\(pullRequestsSeparator)\(pullRequestsRepresentation)"
