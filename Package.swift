cec4fb19d68a71c0d289cdfc5de1a718358c6fc5
Dan Spindelbauer <dan.spindelbauer@babylonhealth.com>
2019-10-17 12:57:05 +0100

    Update links to new GitHub org name

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "5.0.0"),
-        .package(url: "https://github.com/Babylonpartners/ReactiveFeedback", from: "0.6.0"),
+        .package(url: "https://github.com/babylonhealth/ReactiveFeedback", from: "0.6.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0")
     ],
