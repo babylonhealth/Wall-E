2c61a6c0e271f1a7c920939e19c007fb98a308e9
Ilya Puchka <ilya.puchka@babylonhealth.com>
2019-11-25 17:11:27 +0000

    if let instead of !=

--- WallEView/EventMonitor.swift
+++ WallEView/EventMonitor.swift
@@ -1,27 +1,27 @@
 import Cocoa
 
 public class EventMonitor {
-  private var monitor: Any?
-  private let mask: NSEvent.EventTypeMask
-  private let handler: (NSEvent?) -> Void
+    private var monitor: Any?
+    private let mask: NSEvent.EventTypeMask
+    private let handler: (NSEvent?) -> Void
 
-  public init(mask: NSEvent.EventTypeMask, handler: @escaping (NSEvent?) -> Void) {
-    self.mask = mask
-    self.handler = handler
-  }
-
-  deinit {
-    stop()
-  }
-
-  public func start() {
-    monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler)
-  }
-
-  public func stop() {
-    if monitor != nil {
-      NSEvent.removeMonitor(monitor!)
-      monitor = nil
+    public init(mask: NSEvent.EventTypeMask, handler: @escaping (NSEvent?) -> Void) {
+        self.mask = mask
+        self.handler = handler
     }
-  }
+
+    deinit {
+        stop()
+    }
+
+    public func start() {
+        monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler)
+    }
+
+    public func stop() {
+        if let monitor = monitor {
+            NSEvent.removeMonitor(monitor)
+            self.monitor = nil
+        }
+    }
 }
