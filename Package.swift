9ecd199f9f665b1547142cf9ef64d738f112bdf4
David Rodrigues <david.ndh@gmail.com>
2020-01-21 17:40:32 +0000

    Downgrade the version of CryptoSwift.git

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.0.0"),
         .package(url: "https://github.com/Babylonhealth/ReactiveFeedback", from: "0.7.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("1.1.3"))
     ],
     targets: [
         .target(name: "Bot", dependencies: ["ReactiveSwift", "ReactiveFeedback", "CryptoSwift"]),
