9424c9521a1f96d5247e274c277b5efae121f1e3
David Rodrigues <david.ndh@gmail.com>
2020-01-21 17:52:31 +0000

    Move to Swift 5.1 because why not

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
@@ -8,7 +8,7 @@ let package = Package(
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.0.0"),
         .package(url: "https://github.com/Babylonhealth/ReactiveFeedback", from: "0.7.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("1.1.3"))
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.0")
     ],
     targets: [
         .target(name: "Bot", dependencies: ["ReactiveSwift", "ReactiveFeedback", "CryptoSwift"]),
