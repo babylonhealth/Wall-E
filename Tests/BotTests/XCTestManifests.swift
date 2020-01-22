1013758cec464e6676054c8d6fcd495b9dde0d7e
Olivier Halligon <olivier.halligon@babylonhealth.com>
2020-01-22 19:35:43 +0100

    Re-enable all the tests on Linux…

--- Tests/BotTests/XCTestManifests.swift
+++ Tests/BotTests/XCTestManifests.swift
@@ -6,9 +6,12 @@ extension DispatchServiceTests {
     //   `swift test --generate-linuxmain`
     // to regenerate.
     static let __allTests__DispatchServiceTests = [
-//        ("test_adding_new_pull_requests_during_integration", test_adding_new_pull_requests_during_integration),
-//        ("test_creating_new_pull_requests_to_new_target_branch_without_label", test_creating_new_pull_requests_to_new_target_branch_without_label),
-//        ("test_json_queue_description", test_json_queue_description),
+        ("test_adding_new_pull_requests_during_integration", test_adding_new_pull_requests_during_integration),
+        ("test_creating_new_pull_requests_to_new_target_branch_without_label", test_creating_new_pull_requests_to_new_target_branch_without_label),
+        ("test_json_queue_description", test_json_queue_description),
+        ("test_mergeservice_destroyed_if_idle_long_enough", test_mergeservice_destroyed_if_idle_long_enough),
+        ("test_mergeservice_destroyed_when_idle_after_boot", test_mergeservice_destroyed_when_idle_after_boot),
+        ("test_mergeservice_not_destroyed_if_not_idle_long_enough", test_mergeservice_not_destroyed_if_not_idle_long_enough),
         ("test_multiple_pull_requests_with_different_target_branches", test_multiple_pull_requests_with_different_target_branches),
     ]
 }
@@ -81,6 +84,7 @@ extension MergeServiceTests {
         ("test_pull_request_blocked_with_successful_status_pending_checks", test_pull_request_blocked_with_successful_status_pending_checks),
         ("test_pull_request_does_not_fail_prematurely_if_checks_complete_before_adding_the_following_checks", test_pull_request_does_not_fail_prematurely_if_checks_complete_before_adding_the_following_checks),
         ("test_pull_request_fails_integration_after_timeout", test_pull_request_fails_integration_after_timeout),
+        ("test_pull_request_not_included_on_close", test_pull_request_not_included_on_close),
         ("test_pull_request_with_an_initial_unknown_state_with_recover", test_pull_request_with_an_initial_unknown_state_with_recover),
         ("test_pull_request_with_an_initial_unknown_state_without_recover", test_pull_request_with_an_initial_unknown_state_without_recover),
         ("test_pull_request_with_integration_label_and_behind_target_branch", test_pull_request_with_integration_label_and_behind_target_branch),
@@ -109,12 +113,12 @@ extension ResponseTests {
 public func __allTests() -> [XCTestCaseEntry] {
     return [
         testCase(DispatchServiceTests.__allTests__DispatchServiceTests),
-//        testCase(GitHubAPITests.__allTests__GitHubAPITests),
-//        testCase(GitHubDecodingTests.__allTests__GitHubDecodingTests),
-//        testCase(GitHubEventsTests.__allTests__GitHubEventsTests),
-//        testCase(MergeServiceHealthcheckTests.__allTests__MergeServiceHealthcheckTests),
-//        testCase(MergeServiceTests.__allTests__MergeServiceTests),
-//        testCase(ResponseTests.__allTests__ResponseTests),
+        testCase(GitHubAPITests.__allTests__GitHubAPITests),
+        testCase(GitHubDecodingTests.__allTests__GitHubDecodingTests),
+        testCase(GitHubEventsTests.__allTests__GitHubEventsTests),
+        testCase(MergeServiceHealthcheckTests.__allTests__MergeServiceHealthcheckTests),
+        testCase(MergeServiceTests.__allTests__MergeServiceTests),
+        testCase(ResponseTests.__allTests__ResponseTests),
     ]
 }
 #endif
