3a8078075744dc0843a7f7569b047eb740876638
David Rodrigues <david.ndh@gmail.com>
2020-01-22 12:48:50 +0000

    Restore CryptoSwift
    This reverts commit cf7022bf7fd604309cb0375402dc74d773caf263.

--- Package.swift
+++ Package.swift
@@ -7,10 +7,11 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.0.0"),
         .package(url: "https://github.com/Babylonhealth/ReactiveFeedback", from: "0.7.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2")
+        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.0")
     ],
     targets: [
-        .target(name: "Bot", dependencies: ["ReactiveSwift", "ReactiveFeedback"]),
+        .target(name: "Bot", dependencies: ["ReactiveSwift", "ReactiveFeedback", "CryptoSwift"]),
         .target(name: "App", dependencies: ["Bot", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "BotTests", dependencies: ["Bot", "Nimble"])
