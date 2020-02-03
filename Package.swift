db982418f9aa1613e8334e2589b8541336c348ec
Olivier Halligon <olivier.halligon@babylonhealth.com>
2020-02-03 17:40:32 +0000

    Fix Linux compilation issue (hopefully)

--- Package.swift
+++ Package.swift
@@ -5,13 +5,14 @@ let package = Package(
     name: "WALL-E",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/console.git", from: "3.0.0"),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.0.0"),
         .package(url: "https://github.com/Babylonhealth/ReactiveFeedback", from: "0.7.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.0")
     ],
     targets: [
-        .target(name: "Bot", dependencies: ["ReactiveSwift", "ReactiveFeedback", "CryptoSwift"]),
+        .target(name: "Bot", dependencies: ["ReactiveSwift", "ReactiveFeedback", "CryptoSwift", "Logging"]),
         .target(name: "App", dependencies: ["Bot", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "BotTests", dependencies: ["Bot", "Nimble"]),
