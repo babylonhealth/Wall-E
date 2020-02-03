8850a5340555e2e39c4d2c04f2de925c8b231764
Olivier Halligon <olivier.halligon@babylonhealth.com>
2020-02-03 18:31:30 +0100

    Cleanup

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.0.0"),
         .package(url: "https://github.com/Babylonhealth/ReactiveFeedback", from: "0.7.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.0")
     ],
     targets: [
         .target(name: "Bot", dependencies: ["ReactiveSwift", "ReactiveFeedback", "CryptoSwift"]),
@@ -18,4 +18,3 @@ let package = Package(
         .testTarget(name: "AppTests", dependencies: ["App", ])
     ]
 )
-
